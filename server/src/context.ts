c88efa47efc3a5954a9c9b40e9c046c32b5d1628
Mko G <30924553+PerlBug@users.noreply.github.com>
2020-12-11 21:41:07 +1100

    feat: added getAuth to context

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,10 +1,13 @@
 import { getConnection } from "./database/Provider";
+import { IContextType } from "./types";
+import { getAuthPayload } from "./utils/auth";
 
 /**
  * @description holds context for Apollo Server
  */
 
-export const context = async ({req,res}) => {
+export const context = async ({req,res}): Promise<IContextType> => {
+  let payload = await getAuthPayload(req);
   const dbConn = await getConnection();
-  return { dbConn,req,res };
+  return { dbConn,req,res, payload };
 }
